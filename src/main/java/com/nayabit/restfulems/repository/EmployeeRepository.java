078202e2cc502740abcf9214e8e319ae8381b284
Battushig Namsraidorj <battushig.namsraidorj@gmail.com>
2022-11-19 11:46:07 -0600

    create Employee Repository

---
+++ src/main/java/com/nayabit/restfulems/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.nayabit.restfulems.repository;
+
+import com.nayabit.restfulems.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
