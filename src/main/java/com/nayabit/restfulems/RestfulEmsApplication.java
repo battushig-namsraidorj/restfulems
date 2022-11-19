3b1cb6f0dd7f88a07e8e0721d6b1e6d9f6c7f1f6
Battushig Namsraidorj <battushig.namsraidorj@gmail.com>
2022-11-19 11:38:59 -0600

    Initial commit

---
+++ src/main/java/com/nayabit/restfulems/RestfulEmsApplication.java
@@ -0,0 +1,13 @@
+package com.nayabit.restfulems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulEmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulEmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nayabit/restfulems/RestfulEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayabit.restfulems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulEmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
